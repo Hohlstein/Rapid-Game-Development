8be875d123f80d91b2f0d5bffb01e1cb7bc2129c
Katharina Krier <60114161+Hohlstein@users.noreply.github.com>
2023-07-14 20:05:37 +0200

    Update SceneManagement.cs

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -2,12 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
-using System.Linq;
 
 public class SceneManagement :  MonoBehaviour
 {
-
     public static  void changeScene(string newScene)
     {   
         SceneManager.LoadScene(newScene);
