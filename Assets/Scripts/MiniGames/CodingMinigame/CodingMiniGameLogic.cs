d8201bb56f9b1d5990ab9f61637dd0aa24d969c7
Katharina Krier <katharina.krier@web.de>
2023-08-15 13:44:48 +0200

    timer

--- Assets/Scripts/MiniGames/CodingMinigame/CodingMiniGameLogic.cs
+++ Assets/Scripts/MiniGames/CodingMinigame/CodingMiniGameLogic.cs
@@ -3,24 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+
+
 public class CodingMiniGameLogic : MonoBehaviour
 {
     public List<GameObject> codeSnippetlist = new List<GameObject>();
     public List<GameObject> spawnPointlist = new List<GameObject>();
+    public Countdown timer;
+
     private GameObject neededCodeSnippet;
     private System.Random rand = new System.Random();
     private int neededId = 0;
+    private int timer_seconds = 30;
     public GameObject neededCodeSnippetspawnPoint;
     // Start is called before the first frame update
     void Start()
     {
+
             
             neededCodeSnippet = codeSnippetlist[rand.Next(0, codeSnippetlist.Count)];
             neededCodeSnippet = Instantiate(neededCodeSnippet, neededCodeSnippetspawnPoint.transform);
             neededCodeSnippet.GetComponent<Button>().enabled = false;
             neededId = neededCodeSnippet.GetComponent<CodeSnippet>().getId();
           
-            int n = codeSnippetlist.Count;  
+            shuffle();
+            timer.SetRemainingSeconds(timer_seconds);
+            timer.Unfreeze();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void test(int id){
+        if(id == neededId ){
+            
+            shuffle();
+            this.GetComponent<EndMiniGame>().EndNow(100, "Good Job!", "Coding");
+        }
+        else{
+            Debug.Log("Not Found");
+        }
+    }
+
+    private void shuffle(){
+         int n = codeSnippetlist.Count;  
             while (n > 1) {  
                  n--;  
                 int k = rand.Next(n + 1);  
@@ -33,23 +63,6 @@ public class CodingMiniGameLogic : MonoBehaviour
                    codeSnippetlist[i].transform.position = spawnPointlist[i].transform.position;
                  
             }
-
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void test(int id){
-        if(id == neededId ){
-            Debug.Log("Found");
-        }
-        else{
-            Debug.Log("Not Found");
-        }
     }
     
 
